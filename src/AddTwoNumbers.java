a138a14dced458ef1623fcd4f10ca08b3de23ddf
Amr Sadat <amr_sadat@siliconexpert.com>
2023-07-04 09:22:33 +0300

    first commit

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,59 @@
+package src;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode listNode = addTwoNumbers(l1, l2);
+        System.out.println(listNode.val);
+        System.out.println(listNode.next.val);
+        System.out.println(listNode.next.next.val);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode l3 = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+
+            int sum = l1Val + l2Val + carry;
+            carry = sum / 10;
+            int currentDigit = sum % 10;
+
+            ListNode newNode = new ListNode(currentDigit);
+            l3.next = newNode;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+            l3 = l3.next;
+        }
+        if (carry > 0) {
+            ListNode newNode = new ListNode(carry);
+            l3.next = newNode;
+        }
+        return dummy.next;
+    }
+
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+
+
---
+++ src/ClimbingStairs.java
@@ -0,0 +1,13 @@
+package src;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(4));
+        System.out.println(climbStairs(6));
+    }
+    public static int climbStairs(int n) {
+        return 0;
+    }
+
+}
